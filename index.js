ca07184137940f719ea23c2131793f1831b4bdfe
Annie Liao <liaowow@gmail.com>
2020-04-18 14:59:08 -0400

    completed exercise#1

---
+++ index.js
@@ -0,0 +1,14 @@
+/*** DOM Elements ***/
+
+
+/*** Event Listeners ***/
+
+
+/*** Event Handlers ***/
+function js_style() {
+    text.style.fontSize = 20
+    text.style.fontFamily = "Futura"
+    text.style.color = "purple"
+}
+
+/*** Render Functions ***/

f4557050b6d0664d1400aa7b14508345097e9925
Annie Liao <liaowow@gmail.com>
2020-04-27 14:04:04 -0400

    completed exercise#2

--- viking.js
+++ viking.js
@@ -4,4 +4,9 @@
 $("h1").text("DOM DOM DOM DOM manipulation")
 
 /*** EXERCISE 2 ***/
-$(".info-box").append("<ol></ol>")
+// $(".info-box").append("<ol><li>Upcoming One</li><li>Upcoming Two</li></ol>")
+
+$(".info-box").append("<ol><li></li><li></li><li></li></ol>")
+$("ol li").text(function(index) {
+    return "Upcoming Traversal " + (index + 1)
+})
